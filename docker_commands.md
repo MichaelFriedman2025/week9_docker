5e6627a139a8c35ccc37a0650e09244a7f9b4c3c
michael friedman <myklp1013@gmail.com>
2025-12-17 13:05:45 +0200

    complited the test

---
+++ docker_commands.md
@@ -0,0 +1,41 @@
+# Docker Commands
+
+Fill in the Docker commands you used to complete the test.
+
+## Volume
+
+### Create the volume
+
+```bash
+docker volume create fastapi-db
+
+```
+
+## Server 1
+
+### Build the image
+
+```bash
+docker build -t shopping-server1:v1 .
+```
+
+### Run the container
+
+```bash
+docker run --name server1 -v fastapi-db:/app/db -p 8000:8000 shopping-server1:v1
+```
+
+## Server 2
+
+### Build the image
+
+```bash
+docker build -t shopping-server2:v1 .
+```
+
+### Run the container
+
+```bash
+docker run --name server2 -v fastapi-db:/app/db -p 8001:8000 shopping-server2:v1
+```
+
---
+++ server1/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from pathlib import Path
+import json
+import uvicorn
+
+app = FastAPI(title="server1")
+
+DB_PATH = Path("db/shopping_list.json")
+
+class Item(BaseModel):
+    name:str
+    quantity: int
+
+
+@app.get("/items")
+def return_all_items() -> list:
+    with open(DB_PATH, "r") as f:
+        return json.load(f)
+
+@app.post("/items")
+def add_item(item:Item):
+    if not DB_PATH.exists():
+        data = []
+    else:
+        with open(DB_PATH, "r") as f:
+            data = json.load(f)
+    len_data = len(data)
+    data.append({"id":len_data + 1,"name":item.name,"quantity":item.quantity})
+    with open(DB_PATH, "w") as f:
+        json.dump(data, f, indent=4)
+    return {"all items":data}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
